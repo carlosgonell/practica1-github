597994998697b4d14bcb963b3e47cd4689fb0003
carlosgonell <carlitosgonell06@gmail.com>
2025-10-07 17:21:56 -0400

    Create app.js

---
+++ app.js
@@ -0,0 +1,10 @@
+const estudiante = {
+  nombre: "Carlos Gonell",
+  matricula: "20240038",
+  curso: "Electiva 2 - DevOps"
+};
+
+console.log("Hola Mundo");
+console.log(`Estudiante: ${estudiante.nombre} (${estudiante.matricula})`);
+console.log(`Curso: ${estudiante.curso}`);
+console.log(`Fecha/Hora: ${new Date().toLocaleString()}`);
